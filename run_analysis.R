500bb0f41ce70acf62b0b1c581e2b445e1b146a1
Arksutw <arksutw@gmail.com>
2015-05-25 11:26:24 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -52,8 +52,6 @@ traindf<-data.frame(subjectTrain, yTrain, xTrain)
 dataset<-rbind(testdf, traindf)
 
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-
 #Split dataset by subject and acvitity
 splitdatasets<-split(dataset, list(dataset$subjectID, dataset$activityCode), drop=TRUE)
 
